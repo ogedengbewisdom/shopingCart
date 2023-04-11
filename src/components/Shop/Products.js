7faf535a225ed57d2b2546b0ffc63971f9d1b881
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-11 04:33:46 +0100

    maping the dummy data instead of hardcoding productItem component

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,15 +2,38 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
+
+  const DUMMY_DATA = [
+
+    {
+       id: "p1", 
+       price: 5, 
+       title: "My first book", 
+       description: "This is a first product - amazing"
+    }, 
+
+    {
+      id: "p2",
+      price: 10,
+      title: "Second book",
+      description: "This is awesome"
+    }
+  ]
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
+
+        {DUMMY_DATA.map(item => 
         <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
+          key = {item.id}
+          id = {item.id}
+          title = {item.title}
+          price = {item.price}
+          description = {item.description}
         />
+        
+        )}
       </ul>
     </section>
   );
