5259bcd150c04131c53244e54c03c9c589edb474
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-11 04:32:26 +0100

    cartSlice with intitalState of items empty array and addItemToCart & removeItemFromCart reducer

---
+++ src/store/cartSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartInitialState = {
+    items: [],
+    totalQuantity: 0
+}
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: cartInitialState,
+    reducers: {
+        addItemToCart ( state, action ) { 
+            const newItem = action.payload;
+            const existingItem = state.items.find( item => item.id === newItem.id ) 
+            
+// check if item already exist in the array if not add new item if it does exist increase the quantity by 1 and the newPrice to the existing item
+
+            if ( !existingItem ) { 
+                state.items.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    name: newItem.title
+                })
+            } else {
+                existingItem.quantity++;
+                existingItem.totalPrice = existingItem.totalPrice + newItem.price
+            }
+        },
+
+        removeItemFromCart ( state, action ) { 
+            const id = action.payload
+            const existingItem = state.items.find(item => item.id === id)
+            
+            if ( existingItem.quantity === 1 ) {
+                state.items = state.items.filter(item => item.id !== id)
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price
+            }
+        }
+    }
+})
+
+export const cartActions = cartSlice.actions
+
+
+export default cartSlice
