c9d676f459b4a4e1731fd3b443b3ce99e3f27bdf
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-11 04:36:28 +0100

    dispatch cartActions and passing data via props

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,23 @@
+import { useDispatch } from 'react-redux';
 import classes from './CartItem.module.css';
+import { cartActions } from '../../store/cartSlice';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+
+  const dispatch = useDispatch()
+  const { title, quantity, total, price, id } = props.item;
+
+  const addItemToCartHandler = () => {
+    dispatch(cartActions.addItemToCart({
+      id,
+      quantity,
+      price
+    }))
+  }
+
+  const removeItemFromCartHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id))
+  }
 
   return (
     <li className={classes.item}>
@@ -17,8 +33,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemFromCartHandler}>-</button>
+          <button onClick={addItemToCartHandler}>+</button>
         </div>
       </div>
     </li>
