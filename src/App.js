f6b1292e68cfb1e12ae7383831a17fc1abd09642
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-13 20:38:31 +0100

    Handling Http states & feedback with Redux

--- src/App.js
+++ src/App.js
@@ -12,60 +12,102 @@ let isInital = true
 function App() {
   const show = useSelector(state => state.ui.showCart) 
   const cart = useSelector( state => state.cart)
-  const notification = useSelector(state => state.ui.notification)
+  const notification = useSelector( state => state.ui.notification)
 
   const dispatch = useDispatch()
 
+  // useEffect(() => {
+
+   
+  //     const sendData = async () => {
+
+  //    dispatch(uiActions.showNotification({
+  //       status: "pending",
+  //       message: "Sending cart data",
+  //       title: "Sending"
+  //   }))
+
+  //       const response = await fetch(`https://advanceredux-8aa58-default-rtdb.firebaseio.`, {
+  //         method: "PUT",
+  //         body: JSON.stringify(cart)
+  //       })
+        
+  //       if (!response.ok) {
+  //         throw new Error("Something went wrong")
+  //       }
+  //       const responseData = response.json()
+  //       console.log(responseData)
+  //     }
+
+  //     if ( isInital ) {
+  //         isInital = false
+  //         return;
+  //       }
+
+       
+  //      dispatch(uiActions.showNotification({
+  //       status: "success",
+  //       message: "Successfuly sent cart data",
+  //       title: "Sent"
+  //     }))
+    
+
+  //   sendData().catch ((error) => { 
+  //     dispatch(uiActions.showNotification({
+  //       status: "error",
+  //       message: "Failed transaction",
+  //       title: "Error 404"
+  //     }))  
+  //   })
+    
+  // }, [cart, dispatch])
+
   useEffect(() => {
 
-    dispatch(uiActions.showNotification({
-      status: "pending",
-      message: "Sending cart data",
-      title: "Sending"
-    }))
-
-    try {
-      const sendData = async () => {
-        const response = await fetch(`https://advanceredux-8aa58-default-rtdb.firebaseio.`, {
-          method: "PUT",
-          body: JSON.stringify(cart)
-        })
-
-        if ( isInital ) {
-          isInital = false
-          return;
-        }
-        
-        if (!response.ok) {
-          throw new Error("Something went wrong")
-        }
-        const responseData = response.json()
-        console.log(responseData)
-      }
+    const sendCartData = async () => {
 
       dispatch(uiActions.showNotification({
-        status: "success",
-        message: "Successfuly sent cart data",
-        title: "Sent"
+        status: "pending",
+        message: "Sending Cart Data",
+        title: "Sending"
       }))
-
-       sendData() 
+    const response = await fetch(`https://advanceredux-8aa58-default-rtdb.firebaseio.com/cart.json`, {
+      method: "PUT",
+      body: JSON.stringify(cart)
+    })
+    if ( !response.ok ) {
+      throw new Error ("Something went wrong")
     }
 
-    catch (error) { 
+    dispatch(uiActions.showNotification({
+      status: "success",
+      message: "Successfully send Cart data",
+      title: "Success"
+    }))
+    }
+
+    if (  isInital ) {
+      isInital = false
+      return;
+    }
+
+    sendCartData().catch(error => {
       dispatch(uiActions.showNotification({
         status: "error",
-        message: "Failed transaction",
+        message: "Failed to send cart",
         title: "Error 404"
-      }))  
-    }
-    
+      }))
+    })
   }, [cart, dispatch])
 
 
   return (
     <Fragment>
-      {notification && <Notification title={notification.title} status={notification.status} message={notification.message} />}
+      {notification && <Notification 
+      title = {notification.title}
+      status = {notification.status}
+      message = {notification.message}
+      />}
       <Layout>
         {show && <Cart />}
         <Products />
