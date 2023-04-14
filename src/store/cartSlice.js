211b3dd32ae5da06c14db38f1d99abd3ef4823c1
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-15 00:34:56 +0100

    configure createSlice

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { uiActions } from "./uiSlice";
 
 const cartInitialState = {
     items: [],
@@ -48,45 +47,6 @@ const cartSlice = createSlice({
         }
     }
 })
-
-
-export const sendCartData = ( cart ) => {
-    return async (dispatch) => {
-        dispatch(uiActions.showNotification({
-            title: "Sending...",
-            status: "pending",
-            message: "sending cart data!"
-        }))
-
-        const sendRequest = async () => {
-            const response = await fetch(`https://advanceredux-8aa58-default-rtdb.firebaseio.com/cart.json`, {
-                method: "PUT",
-                body: JSON.stringify(cart)
-            })
-
-            if ( !response.ok ) {
-                throw new Error("Error 404")
-            }
-        }
-
-        try {
-            await sendRequest()
-            dispatch(uiActions.showNotification({
-                title: "Success",
-                status: "success",
-                message: "sent cart data!"
-            }))
-        } 
-
-        catch(error) {
-            dispatch(uiActions.showNotification({
-                title: "Error 404",
-                message: error.message,
-                status: "error"
-            }))
-        }
-    }
-}
 
 
 export const cartActions = cartSlice.actions
